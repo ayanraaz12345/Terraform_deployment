09a1de4ed1c45ea37bca332a9b826b3dc5da5e63
ayanraaz12345 <152529666+ayanraaz12345@users.noreply.github.com>
2025-12-09 17:15:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Get default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Get default VPC subnets (required for EC2 placement)
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
 ############ Security Group ############
 resource "aws_security_group" "ec2_sg" {
-  name        = "ec2_security_group"
-  description = "Allow Nginx and SSH"
-  vpc_id      = data.aws_vpc.default.id
+  name   = "ec2_security_group"
+  vpc_id = "default"
 
   ingress {
     from_port   = 22
@@ -38,18 +24,17 @@ resource "aws_security_group" "ec2_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 ############ EC2 Instance ############
 resource "aws_instance" "app_ec2" {
-  ami                         = "ami-0c02fb55956c7d316"   # Valid Amazon Linux 2 AMI (us-east-1)
-  instance_type               = "t2.micro"
-  key_name                    = var.key_name
-  subnet_id                   = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  ami                    = "" # Amazon Linux 2
+  instance_type          = "t2.micro"
+  key_name               = var.key_pair
+  security_groups        = [aws_security_group.ec2_sg.name]
 
   tags = {
     Name = "github-actions-ec2"
@@ -62,13 +47,4 @@ yum install -y nginx
 systemctl enable nginx
 systemctl start nginx
 EOF
-}
-
-############ Outputs ############
-output "public_ip" {
-  value = aws_instance.app_ec2.public_ip
-}
-
-output "url" {
-  value = "http://${aws_instance.app_ec2.public_ip}"
 }
