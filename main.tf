4c09a9d9df6edc84b1a647bd63ef864303cb595f
ayanraaz12345 <152529666+ayanraaz12345@users.noreply.github.com>
2025-12-09 17:34:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "ec2_sg" {
 ############ EC2 Instance ############
 resource "aws_instance" "app_ec2" {
   ami                    = "ami-0ecb62995f68bb549"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = var.key_pair
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
