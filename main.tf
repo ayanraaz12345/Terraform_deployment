3f149c64708ef42827146051d9ed82dba2172423
ayanraaz12345 <152529666+ayanraaz12345@users.noreply.github.com>
2025-12-10 01:36:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,8 +48,8 @@ resource "aws_instance" "app_ec2" {
 
   user_data = <<EOF
 #!/bin/bash
-yum update -y
-yum install -y nginx
+apt update -y
+apt install -y nginx
 systemctl enable nginx
 systemctl start nginx
 EOF
